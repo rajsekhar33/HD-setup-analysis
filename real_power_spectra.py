9f2b23c7ad598d37c370c93cd02fb9ac564f759f
rajsekhar33 <rajsekhar1996@gmail.com>
2017-09-29 18:23:51 +0530

    Modified with correct definition of E(k)

--- real_power_spectra.py
+++ real_power_spectra.py
@@ -7,28 +7,28 @@ import os, sys
 from collections import Counter
 import time
 from scipy.optimize import curve_fit
-sys.path.append("$PLUTO_DIR/Tools/pyPLUTO")
+sys.path.append("/home/rajsekhar/PLUTO41_old/Tools/pyPLUTO")
 import pyPLUTO as pp
 
 #Compute how long the simulation takes
 start_time = time.time()
 #bin_size denotes the division of k space into bins of size 2*pi*bin_size
-bin_size=2
+bin_size=4
 
 #n is an array that stores the size of the simulation domain
 n=np.array([128,128,128])
 
 #Declare all parameters and filenames, file location
 
-filedir="/home/rajsekhar/PLUTO41_old/3D_turb/Tau_c_8/"
+filedir="/home/rajsekhar/PLUTO41_old/3D_turb/Tau_c_20/128/"
 
-for filenumber in xrange(15,16):
+for filenumber in xrange(3,4):
 
     #Load data files using pp.pload
 
     D=pp.pload(filenumber,w_dir=filedir)
 
-    #Perform fourier transform from complex to complex(I didn't do real to complex, because the later steps get less complicated if I do it this way
+    #Perform fourier transform from real to complex
 
     Vk1=(float(1)/np.prod(n))*np.fft.rfftn(D.vx1,s=n)
     Vk2=(float(1)/np.prod(n))*np.fft.rfftn(D.vx2,s=n)
@@ -47,8 +47,8 @@ for filenumber in xrange(15,16):
     #Write k_sq as a functional 3d array, with value at each element given by (nx/2-i)^2+(ny/2-j)^2+(k)^2
     k_sq=np.fromfunction(lambda i,j,k:(i-n[0]/2)**2+(j-n[1]/2)**2+k**2,np.shape(Vk1))
 
-    #Energy density is given by k^2 * Vk^2
-    E_k=2*np.multiply(Vk_sq,k_sq)
+    #Energy density is given by 2 * Vk^2
+    E_k=2*Vk_sq
 
     #Flatten E_k and k now, and store them in a single  2D array
 
@@ -66,16 +66,15 @@ for filenumber in xrange(15,16):
         c[k] += v
 
     #K_rad takes non-empty elements of c, and stores the corresponding k(taking square root of k_sq) and E(k) values
-    K_rad=[[2*np.pi*np.sqrt(var),c[var]] for var in c if var]
+    K_rad=[[2*np.pi*np.sqrt(var),c[var]/(float(2)*np.pi*bin_size)] for var in c if var]
     K_rad=np.array(K_rad)
     #the previous procedure leaves out the value corresponding to k=0, so we insert it
-    K_rad=np.insert(K_rad,0,c[0],axis=0)
-    K_E_comp=np.multiply(K_rad[:,1],np.power(K_rad[:,0],5/3))
+    K_rad=np.insert(K_rad,0,c[0]/(float(2)*np.pi*bin_size),axis=0)
     
     #Take bins of a certain size, and add up values corresponding to thse bins
 
     index=0
-    K_avg=np.empty([0,3],dtype=float)
+    K_avg=np.empty([0,2],dtype=float)
     for i in xrange(0,int((1/bin_size)*np.sqrt(np.sum(np.square(n/2))))):
         k_min=2*bin_size*np.pi*i
         k_max=2*bin_size*np.pi*(i+1)
@@ -84,13 +83,14 @@ for filenumber in xrange(15,16):
         for j in xrange(index,np.size(K_rad[:,0])):
             if K_rad[:,0][j]<k_max:
                 E+=K_rad[:,1][j]
-                E_comp+=K_E_comp[j]
             else:
-                K_avg=np.append(K_avg,[[k_min,E,E_comp]],axis=0)
+                K_avg=np.append(K_avg,[[k_min,E]],axis=0)
                 index=j+1
                 break
-        
+    K_E_comp=np.multiply(K_avg[:,1],np.power(K_avg[:,0],5/3))
+    K_avg=np.transpose(np.vstack((K_avg[:,0],K_avg[:,1],K_E_comp)))
     np.savetxt(filedir+'power_spectrum_'+str(filenumber)+'.txt',K_avg)
+    np.savetxt('power_spectrum_'+str(filenumber)+'.txt',K_avg)
     
 #Curve fitting
     #Plot the data 
@@ -104,6 +104,8 @@ for filenumber in xrange(15,16):
     plt.title('Compensated E(k) vs k for t='+str(filenumber))
 
     plt.savefig(filedir+'log_E_k_compensated'+str(filenumber)+'.png')
+    plt.savefig('log_E_k_compensated'+str(filenumber)+'.png')
+    
     #This is to plot the original power spectrum, without any compensation
     plt.figure()
     plt.loglog(K_avg[:,0],K_avg[:,1])
@@ -113,5 +115,6 @@ for filenumber in xrange(15,16):
 
     plt.title('E(k) vs k for t='+str(filenumber))
     plt.savefig(filedir+'log_E_k'+str(filenumber)+'.png')
+    plt.savefig('log_E_k'+str(filenumber)+'.png')
 
     print("--- %s seconds ---" % (time.time() - start_time))

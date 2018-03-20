8b08ce8392b11206c5e0891d36d80f074c494ff2
Rajsekhar Mohapatra <rajsekhar1996@gmail.com>
2018-03-20 11:47:28 +0530

    Added mach number and temp dist plots

--- del-rho-plots/density_perturb.py
+++ del-rho-plots/density_perturb.py
@@ -12,11 +12,11 @@ UNIT_VELOCITY= (1.e8)
 UNIT_LENGTH =  (CONST_pc*40.e3)
 UNIT_TIME=UNIT_LENGTH/UNIT_VELOCITY/(3.15e13)
 
-no_files=10
+no_files=13
 #Load data files
-wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'thermal_heating/256/tabulated_cooling/F1e-3/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-3/k12/','thermal_heating/256/tabulated_cooling/F1e-2/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-2/k12/', 'thermal_heating/256/tabulated_cooling/F1e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-1/k12/','thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'turb_perturb/F1e-3/k12/', 'turb_perturb/F1e-2/k12/', 'no_turb/')
+wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-2/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-3/k0-2/', 'thermal_heating/256/tabulated_cooling/F5e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-2/k12/', 'thermal_heating/256/tabulated_cooling/F1e-3/k12/', 'no_turb/', 'turb_perturb/F1e-2/k12/', 'turb_perturb/F1e-3/k12/')
 
-labels=('Ckl', 'Ckh', 'HCkl4', 'HCkh4',  'HCkl3', 'HCkh3',  'HCkl2', 'HCkh2', 'HCkl1', 'DHCkh2', 'DHCkh1', 'DHC')
+labels=('Ckl', 'Ckh', 'HCkl1', 'HCkl2', 'HCkl3', 'HCkl4', 'HCkh1', 'HCkh2', 'HCkh3', 'HCkh4', 'DHC', 'DHCkh1', 'DHCkh2')
 
 fig, ax = plt.subplots()
 for i1 in xrange(0,no_files):
--- mach_temp_dist/mach-temp-dist.py
+++ mach_temp_dist/mach-temp-dist.py
@@ -12,15 +12,20 @@ UNIT_VELOCITY= (1.e8)
 UNIT_LENGTH =  (CONST_pc*40.e3)
 UNIT_TIME=UNIT_LENGTH/UNIT_VELOCITY/(3.15e13)
 nx,ny,nz=256,256,256
-no_files=13
+#no_files=13
 #Load data files
-wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-2/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-3/k0-2/', 'thermal_heating/256/tabulated_cooling/F5e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-2/k12/', 'thermal_heating/256/tabulated_cooling/F1e-3/k12/', 'no_turb/', 'turb_perturb/F1e-2/k12/', 'turb_perturb/F1e-3/k12/')
+#wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-2/k0-2/', 'thermal_heating/256/tabulated_cooling/F1e-3/k0-2/', 'thermal_heating/256/tabulated_cooling/F5e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-1/k12/', 'thermal_heating/256/tabulated_cooling/F1e-2/k12/', 'thermal_heating/256/tabulated_cooling/F1e-3/k12/', 'no_turb/', 'turb_perturb/F1e-2/k12/', 'turb_perturb/F1e-3/k12/')
+#labels=('Ckl', 'Ckh', 'HCkl1', 'HCkl2', 'HCkl3', 'HCkl4', 'HCkh1', 'HCkh2', 'HCkh3', 'HCkh4', 'DHC', 'DHCkh1', 'DHCkh2')
+#t_mp=(2, 45, 4, 10, 14, 21, 0, 33, 51, 30, 79, 35, 36)
+#t_turb=(1.2, 4.8, 2, 4, 6, 10.2, 0, 6, 12, 12, 2, 10.2, 10.2)
+#step_size=(0.2, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 0.2)
 
-labels=('Ckl', 'Ckh', 'HCkl1', 'HCkl2', 'HCkl3', 'HCkl4', 'HCkh1', 'HCkh2', 'HCkh3', 'HCkh4', 'DHC', 'DHCkh1', 'DHCkh2')
-
-t_mp=(2, 45, 4, 10, 14, 21, 0, 33, 51, 30, 79, 35, 36)
-t_turb=(1.2, 4.8, 2, 4, 6, 10.2, 0, 6, 12, 12, 40, 10.2, 10.2)
-step_size=(0.2, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 0.2)
+no_files=3
+wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'no_turb/')
+labels=('Ckl', 'Ckh', 'DHC')
+t_mp=(19, 45, 79)
+t_turb=(1.2, 4.8, 2)
+step_size=(0.2, 0.2, 0.2)
 
 fig, ax = plt.subplots()
 for i1 in xrange(0,no_files):
@@ -42,7 +47,8 @@ box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width, box.height])
 # Put a legend to the bottom of the current axis
 ax.legend(loc='center left', bbox_to_anchor=(0.8, 0.4), ncol=1)
-plt.savefig('mach-dist.png',dpi=250)
+#plt.savefig('mach-dist.png',dpi=250)
+plt.savefig('mach-dist-extremes.png',dpi=250)
 plt.close()
 
 fig, ax = plt.subplots()
@@ -57,7 +63,7 @@ for i1 in xrange(0,no_files):
 fig.set_size_inches(6, 4)
 ax.set_xscale('log')
 ax.set_yscale('log')
-ax.set_xlabel(r'$\left<T\right>')
+ax.set_xlabel(r'$\left<T\right>$')
 ax.set_ylabel(r'Number density')
 ax.set_title(r'Number density vs $\left<T\right>$')
 # Shrink current axis by 20%
@@ -65,6 +71,7 @@ box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width, box.height])
 # Put a legend to the bottom of the current axis
 ax.legend(loc='center left', bbox_to_anchor=(0.8, 0.4), ncol=1)
-plt.savefig('temp-dist.png',dpi=250)
+#plt.savefig('temp-dist.png',dpi=250)
+plt.savefig('temp-dist-extremes.png',dpi=250)
 plt.close()
 

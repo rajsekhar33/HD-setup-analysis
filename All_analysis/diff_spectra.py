31ff7974db55ff1709f2774226aa542880068a7b
Rajsekhar Mohapatra <rajsekhar1996@gmail.com>
2018-03-23 13:08:36 +0530

    Modified plots

--- All_analysis/diff_spectra.py
+++ All_analysis/diff_spectra.py
@@ -5,11 +5,16 @@ import time
 from scipy.optimize import curve_fit
 from scipy import stats
 import pylab as plot
-params = {'legend.fontsize':8.0,
+plt.style.use('classic')
+params = {'legend.fontsize':10.0,
           'legend.handlelength': 0.5}
+plt.rcParams['axes.linewidth'] = 1.5
+plt.rcParams['xtick.major.size'] = 8
+plt.rcParams['xtick.minor.size'] = 4
+plt.rcParams['ytick.major.size'] = 6
+plt.rcParams['ytick.minor.size'] = 3
+plt.rcParams['ytick.minor.size'] = 3
 plot.rcParams.update(params)
-
-
 
 #Compute how long the simulation takes
 start_time = time.time()
@@ -46,7 +51,7 @@ for i in xrange(0, start.size):
 	   k1[filenumber-start[i]]=datax1[:,0]
 	   Ek1[filenumber-start[i]]=datax1[:,1]+datax2[:,1]+datax3[:,1]
 #Calculate average energy injection
-	   epsilon=np.average(data1[(data1[:,0]>time_step*(filenumber-start[i]))*(data1[:,0]<time_step*(filenumber+1-start[i]))][:,1])
+	   epsilon=np.average(data1[(data1[:,0]>time_step*(filenumber-start[i]))*(data1[:,0]<time_step*(filenumber-start[i])+2e-1)][:,1])
 	   print epsilon
 #Calculate average speed of sound
   	   cs=np.average(data1[(data1[:,0]>time_step*(filenumber-start[i]))*(data1[:,0]<time_step*(filenumber+1-start[i]))][:,2])
@@ -82,26 +87,25 @@ for i in xrange(0, start.size):
 	del_Ek_comp2=np.std(Ekcomp2,0)
 #Here we plot the compensated power spectrum, multiplying E(k) with k^(5/3)
 	if (i==2 or i==0): 
-		ax1.errorbar(k1[1:-10],Ek_comp1[1:-10],yerr=del_Ek_comp1[1:-10],fmt='*-', label=r'$\epsilon^{-2/3}k^{5/3}V_k^2$, $\mathcal{M}=$'+str(mach[i]))
-		ax1.errorbar(k2[1:-10],Ek_comp2[1:-10],yerr=del_Ek_comp2[1:-10],fmt='d-', label=r'$\epsilon^{-2/3}k^{5/3}\rho_k^2$, $\mathcal{M}= =$'+str(mach[i]))
+		ax1.errorbar(k1[1:-10],Ek_comp1[1:-10],yerr=del_Ek_comp1[1:-10],fmt='*', label=r'$\epsilon_v^{-2/3}k^{5/3}E_v(k)$, $\mathcal{M}=$'+str(mach[i]), markeredgecolor='None')
+		ax1.errorbar(k2[1:-10],Ek_comp2[1:-10],yerr=del_Ek_comp2[1:-10],fmt='d', label=r'$\epsilon_v^{-2/3}k^{5/3}E_{\rho}(k)$, $\mathcal{M}= $'+str(mach[i]), markeredgecolor='None')
 	#Plot ratio
 	ax2.plot(k1[1:-10],Ek2[1:-10]/Ek1[1:-10], label='$\mathcal{M}=$'+str(mach[i]))
 ax1.set_yscale('log')
 ax1.set_xscale('log')
-ax1.tick_params(direction='out', length=6, width=2, labeltop=True)
 #ax1.set_xlabel('k')
-ax1.set_ylabel('E(k)*$k^{5/3}*\epsilon_V^{-2/3}$')
+ax1.set_ylabel('$E(k)k^{5/3}\epsilon_V^{-2/3}$')
 # Put a legend to the right of the current axis
 ax1.legend(loc='lower right', bbox_to_anchor=(0.88, 0.0), ncol=2)
 #ax1.set_title(r'Compensated $E_{\rho}(k)$ and $E_V(k)$ vs k for $256^3$' )
+ax1.set_xlim(1e1, 2e3)
 
 ax2.set_yscale('log')
 ax2.set_xscale('log')
-ax2.tick_params(direction='out', length=6, width=2, labeltop=True)
 ax2.set_xlabel('k')
 ax2.set_ylabel(r'$\frac{\rho_k^2}{\left<\rho\right>^2}/\frac{V_k^2}{c_s^2}$')
-ax2.set_ylim(2.e-3,)
-ax2.legend(loc='center left', bbox_to_anchor=(0, 0.95), ncol=6)
+ax2.set_ylim(2.e-3,3e-1)
+ax2.legend(loc='center left', bbox_to_anchor=(0, 0.9), ncol=3)
 #ax2.set_title(r'Ratio of velocity and density power spectra' )
 plt.savefig('Ratio_rho-vel_lowk_256.png',dpi=250)
 
--- rho-mach/density_mach_256.py
+++ rho-mach/density_mach_256.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab as plot
-params = {'legend.fontsize':7.5,
-          'legend.handlelength': 0.5}
+import seaborn as sns
+
+plt.style.use('classic')
+params = {'legend.fontsize':8,
+          'legend.handlelength': 1}
+plt.rcParams['axes.linewidth'] = 0.5
+plt.rcParams['xtick.major.size'] = 8
+plt.rcParams['xtick.minor.size'] = 4
+plt.rcParams['ytick.major.size'] = 6
+plt.rcParams['ytick.minor.size'] = 3
+plt.rcParams['ytick.minor.size'] = 3
 plot.rcParams.update(params)
 
 #Declare all parameters and filenames, file location
@@ -11,12 +20,18 @@ plot.rcParams.update(params)
 amp=np.array((0.020,0.1,0.9,2.5))
 t_start=np.array((5.0,2.0,1.0,0.5))
 #t_start sets time at which statistical equilibrium has been reached
+
+NUM_COLORS = 20
+LINE_STYLES = ['solid', 'dashed', 'dashdot', 'dotted']
+NUM_STYLES = len(LINE_STYLES)
+
+clrs = sns.color_palette('husl', n_colors=NUM_COLORS)  # a list of RGB tuples
+
 fig, ax = plt.subplots()
 for i1 in xrange(0,np.shape(amp)[0]):
 #	fig, ax = plt.subplots(1)
 	filedir='/mnt/lustre/ug4/ugrajs/fiducial_runs/256/amp'+str(int(amp[i1]*1000)).rjust(4,'0')+'/'
 	file=filedir+'pluto_hst.out'
-	fname = open(file,'rt')
 	data = np.loadtxt(file, skiprows=1)
 	#Load data
 	for i in xrange(0,data.shape[0]):
@@ -25,9 +40,11 @@ for i1 in xrange(0,np.shape(amp)[0]):
 	data=data[:][i:]
 	#Ignore data before statistical equilibrium state
 	#Plot the data 
-	ax.plot(data[:,11],1.5*data[:,12],label=r'$\frac{1.5\left<\delta\rho\right>_{rms}}{\left<\rho\right>}$, $A_{turb}=$'+str(amp[i1]))
-	ax.plot(data[:,11],data[:,13],label=r'$\frac{\left<\delta P\right>_{rms}}{\left< P\right>}$, $A_{turb}=$'+str(amp[i1]))
-fig.set_size_inches(6, 4)
+	line1 =ax.plot(data[:,11],1.5*data[:,12],label=r'$\frac{1.5\left<\delta\rho\right>_{rms}}{\left<\rho\right>}$, $A_{turb}=$'+str(amp[i1]))
+	line1[0].set_color(np.random.rand(3))
+	line2=ax.plot(data[:,11],data[:,13],label=r'$\frac{\left<\delta P\right>_{rms}}{\left< P\right>}$, $A_{turb}=$'+str(amp[i1]))
+	line2[0].set_color(np.random.rand(3))
+fig.set_size_inches(6, 4.5)
 ax.set_xlabel(r'$\left< \mathcal{M}\right>_{rms}$')
 ax.set_ylabel(r'$\frac{1.5\left<\delta\rho\right>_{rms}}{\left<\rho\right>}$, $\frac{\left<\delta P\right>_{rms}}{\left< P\right>}$')
 x1=np.arange(0.3,0.8,0.002)
@@ -37,10 +54,12 @@ ax.plot(y,0.5*y**1,label=r'$\left< \mathcal{M}\right>_{rms}$')
 ax.set_yscale('log')
 ax.set_xscale('log')
 ax.set_title(r'$\frac{\left<\delta\rho\right>_{rms}}{\left<\rho\right>}$ and $\frac{\left<\delta P\right>_{rms}}{\left< P\right>}$  vs $\left< \mathcal{M}\right>_{rms}$')
-ax.set_xlim(0.3,)
+ax.set_xlim(0.3,2)
+ax.set_ylim(0.04,1)
 # Shrink current axis by 20%
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width, box.height])
 # Put a legend to the bottom of the current axis
-ax.legend(loc='center left', bbox_to_anchor=(0.7, 0.4), ncol=1)
+ax.legend(loc='lower right', bbox_to_anchor=(1.0, 0.), ncol=2)
 plt.savefig('rho-mach-256.png',dpi=250)
+

7e6331b56318b0b5ac9e25334fdefb096538b698
Rajsekhar Mohapatra <rajsekhar1996@gmail.com>
2018-04-05 16:57:37 +0530

    Added new temp and mach distribution plots

--- mach_temp_dist/mach-temp-dist.py
+++ mach_temp_dist/mach-temp-dist.py
@@ -28,12 +28,12 @@ nx,ny,nz=256,256,256
 #t_turb=(1.2, 4.8, 2, 4, 6, 10.2, 6, 6, 12, 12, 2, 10.2, 10.2)
 #step_size=(0.2, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 0.2)
 
-no_files=2
-wdir=('thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F5e-1/k12/')
-labels=('HCkl', 'HCkh')
-t_mp=(6., 30 )
-t_turb=(2., 4.8)
-step_size=(0.2, 0.2)
+no_files=3
+wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/', 'no_turb/2e-3/')
+labels=('Tl', 'Th', 'Q')
+t_mp=(2., 45., 25.2)
+t_turb=(1.2, 4.8, 7.8)
+step_size=(0.2, 0.2, 0.2)
 
 #Initialise the figure
 fig, (ax1, ax2) = plt.subplots(2, 1)
@@ -41,14 +41,14 @@ for i1 in xrange(0,no_files):
 	filedir='/mnt/lustre/ug4/ugrajs/cooling/'+wdir[i1]
 	file=filedir+'mach'+str(int(t_turb[i1]/step_size[i1]+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax1.plot(data[:,0],data[:,1]/(float(nx*ny*nz)),label=labels[i1]+' t='+str(int(t_turb[i1]*UNIT_TIME))+'Myr')
+	ax1.plot(data[:,0],data[:,1]/(float(nx*ny*nz)),label=labels[i1]+', t='+str(int(t_turb[i1]*UNIT_TIME))+'Myr')
 	file=filedir+'mach'+str(int(t_mp[i1]/step_size[i1]+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax1.plot(data[:,0],data[:,1]/(float(nx*ny*nz)),label=labels[i1]+' t='+str(int(t_mp[i1]*UNIT_TIME))+'Myr')
+	ax1.plot(data[:,0],data[:,1]/(float(nx*ny*nz)),label=labels[i1]+', t='+str(int(t_mp[i1]*UNIT_TIME))+'Myr')
 fig.set_size_inches(6, 7)
 ax1.set_xscale('log')
 ax1.set_yscale('log')
-ax1.set_xlim(1e-2, 5e1)
+ax1.set_xlim(5e-3, 1e1)
 ax1.set_ylim(1e-6, )
 ax1.set_xlabel(r'$\mathcal{M}$')
 ax1.set_ylabel(r'Number fraction')
@@ -56,7 +56,7 @@ ax1.set_ylabel(r'Number fraction')
 box = ax1.get_position()
 ax1.set_position([box.x0, box.y0, box.width, box.height])
 # Put a legend to the bottom of the current axis
-ax1.legend(loc='lower left', bbox_to_anchor=(0.25, 0.0), ncol=2)
+ax1.legend(loc='lower left', ncol=3)
 #plt.savefig('mach-dist.png',dpi=250)
 
 for i1 in xrange(0,no_files):
@@ -69,15 +69,15 @@ for i1 in xrange(0,no_files):
 	ax2.plot(data[:,0],data[:,1]/(float(nx*ny*nz)),label=labels[i1]+', t='+str(int(t_mp[i1]*UNIT_TIME))+'Myr')
 ax2.set_xscale('log')
 ax2.set_yscale('log')
-ax2.set_xlim(1e5, 3e8)
+ax2.set_xlim(1e5, 4e7)
 ax2.set_ylim(1e-6, )
 ax2.set_xlabel(r'$T$')
 ax2.set_ylabel(r'Number fraction')
 box = ax2.get_position()
 ax2.set_position([box.x0, box.y0, box.width, box.height])
 # Put a legend to the bottom of the current axis
-ax2.legend(loc='lower left', bbox_to_anchor=(0.25, 0.0), ncol=2)
+#ax2.legend(loc='lower left', ncol=3)
 #plt.savefig('temp-dist.png',dpi=250)
-plt.savefig('mach-temp-dist-HC.png',dpi=250, mode="expand", borderaxespad=0.)
+plt.savefig('mach-temp-dist-extremes.png',dpi=250, mode="expand", borderaxespad=0.)
 plt.close()
 

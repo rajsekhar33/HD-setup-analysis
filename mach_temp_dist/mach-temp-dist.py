fa6033b2ac3d02e032c681ebfa50a25fc25e694d
Rajsekhar Mohapatra <rajsekhar1996@gmail.com>
2018-05-17 16:19:36 +0530

    Edited mach-temp plots

--- mach_temp_dist/mach-temp-dist.py
+++ mach_temp_dist/mach-temp-dist.py
@@ -29,78 +29,87 @@ nx,ny,nz=256,256,256
 #step_size=(0.2, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 1.0, 0.2, 0.2, 0.2, 0.2)
 
 #wdir=('thermal_heating/256/tabulated_cooling/F5e-1/k0-2/', 'thermal_heating/256/tabulated_cooling/F5e-1/k12/')
-#wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/')
-wdir=('no_turb/2e-1/', 'turb_perturb//DkHC2e-1/', 'turb_perturb/DBh2e-1/')
+wdir=('tabulated_cooling/256/k0-2/', 'tabulated_cooling/256/k12/')
+#wdir=('no_turb/2e-1/', 'turb_perturb//DkHC2e-1/', 'turb_perturb/DBh2e-1/F5e-1/')
 
 #labels=('Bl', 'Bh')
-#labels=('Tl', 'Th')
-labels=('QD', 'TDh', 'BDh')
+labels=('Tl', 'Th')
+#labels=('QD', 'TDh', 'BDh')
+
+#num_plots=2
+num_plots=2
+#num_plots=3
+
+distM = [None] * (num_plots*2)
+distT = [None] * (num_plots*2)
 
 #t_mp=(10., 42.)
-#t_mp=(10., 51.)
-t_mp=(10.4, 42., 15.)
+t_mp=(10., 51.)
+#t_mp=(10.4, 42., 15.)
 
 #t_turb=(1., 4.8)
-#t_turb=(0.8, 4.8)
-t_turb=(0.2, 10.2, 3.)
+t_turb=(0.8, 4.8)
+#t_turb=(0.2, 10.2, 3.)
 
 step_size=np.array((0.2))#, 0.2, 0.2))
 
 #Initialise the figure
 fig, (ax1, ax2) = plt.subplots(2, 1)
 
-for i in xrange(2):
-
+for i in xrange(num_plots):
+	j=i
 	filedir='/mnt/lustre/ug4/ugrajs/cooling/'+wdir[i]
 	file=filedir+'mach'+str(int(t_turb[i]/step_size+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax1.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), label=labels[i]+', t='+str(int(t_turb[i]*UNIT_TIME))+' Myr')
+	distM[j], =ax1.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), label=labels[i]+', t='+str(int(t_turb[i]*UNIT_TIME))+' Myr')
 	file=filedir+'mach'+str(int(t_mp[i]/step_size+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax1.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), dashes=[30, 5, 10, 5], label=labels[i]+', t='+str(int(t_mp[i]*UNIT_TIME))+' Myr')
+	distM[j+num_plots], =ax1.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), dashes=[30, 5, 10, 5], label=labels[i]+', t='+str(int(t_mp[i]*UNIT_TIME))+' Myr')
 	fig.set_size_inches(6, 7)
 	ax1.set_xscale('log')
 	ax1.set_yscale('log')
 	ax1.set_xlim(1e-2, 1e1)
-	ax1.set_ylim(1e-4, )
+	ax1.set_ylim(1e-4, 2e-1)
 	ax1.set_xlabel(r'$\mathcal{M}$')
 	ax1.set_ylabel(r'Volume fraction')
 	# Shrink current axis by 20%
 	box = ax1.get_position()
 	ax1.set_position([box.x0, box.y0, box.width, box.height])
-	# Put a legend to the bottom of the current axis
-	ax1.legend(loc='lower center', ncol=2)
 	#plt.savefig('mach-dist.png',dpi=250)
 	ax1.tick_params(axis='both', which='major', direction='out', length=6, width=0.5, top=True, right=True)
 	ax1.tick_params(axis='both', which='minor', direction='out', length=3, width=0.25, top=True, right=True)
 	ax1.grid(color='grey', linestyle='-', linewidth=0.2)
+	
 
 
 	filedir='/mnt/lustre/ug4/ugrajs/cooling/'+wdir[i]
 	file=filedir+'temp'+str(int(t_turb[i]/step_size+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax2.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), label=labels[i]+', t='+str(int(t_turb[i]*UNIT_TIME))+' Myr')
+	distT[j], =ax2.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), label=labels[i]+', t='+str(int(t_turb[i]*UNIT_TIME))+' Myr')
 	file=filedir+'temp'+str(int(t_mp[i]/step_size+0.1)).rjust(4,'0')+'.txt'
 	data = np.loadtxt(file, skiprows=1, usecols=(0,1))
-	ax2.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), dashes=[30, 5, 10, 5], label=labels[i]+', t='+str(int(t_mp[i]*UNIT_TIME))+' Myr')
+	distT[j+num_plots], =ax2.plot(data[:,0], data[:,1]/(float(nx*ny*nz)), dashes=[30, 5, 10, 5], label=labels[i]+', t='+str(int(t_mp[i]*UNIT_TIME))+' Myr')
 	ax2.set_xscale('log')
 	ax2.set_yscale('log')
 	ax2.set_xlim(1e5, 1e8)
-	ax2.set_ylim(1e-4, )
+	ax2.set_ylim(1e-4, 2e-1)
 	ax2.set_xlabel(r'$T$')
 	ax2.set_ylabel(r'Volume fraction')
 	box = ax2.get_position()
 	ax2.set_position([box.x0, box.y0, box.width, box.height])
 	# Put a legend to the bottom of the current axis
-	ax2.legend(loc='lower center', ncol=2)
+	ax2.legend(loc='upper left', ncol=2, fancybox=True, framealpha=0.)
 	#plt.savefig('temp-dist.png',dpi=250)
 	ax2.tick_params(axis='both', which='major', direction='out', length=6, width=0.5, top=True, right=True)
 	ax2.tick_params(axis='both', which='minor', direction='out', length=3, width=0.25, top=True, right=True)
 	ax2.grid(color='grey', linestyle='-', linewidth=0.2)
 
+distM_legend=ax1.legend(handles=distM, loc='upper left', ncol=2, fancybox=True, framealpha=0.)
+distT_legend=ax2.legend(handles=distT, loc='upper left', ncol=2, fancybox=True, framealpha=0.)
+
 #plt.savefig('mach-temp-dist-B.png',dpi=250, mode="expand", borderaxespad=0.)
-#plt.savefig('mach-temp-dist-T.png',dpi=250, mode="expand", borderaxespad=0.)
-plt.savefig('mach-temp-dist-D.png',dpi=250, mode="expand", borderaxespad=0.)
+plt.savefig('mach-temp-dist-T.png',dpi=250, mode="expand", borderaxespad=0.)
+#plt.savefig('mach-temp-dist-D.png',dpi=250, mode="expand", borderaxespad=0.)
 
 plt.close()
 
